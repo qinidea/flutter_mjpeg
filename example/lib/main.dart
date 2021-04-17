39dfa27eaed8ba4fd871e0cc6115b146d05dc14d
Lan <lanxianqin@mhearts.com>
2021-04-17 20:14:12 +0800

    video ok mouse ok keyboard ongoing

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,14 @@
+// @dart=2.9
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_mjpeg/flutter_mjpeg.dart';
+import 'package:udp/udp.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,9 +26,59 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends HookWidget {
+  WebSocket _webSocket;
+
+  Mjpeg mjpeg;
+
+  MyHomePage() {
+    print('=============MyHomePage');
+
+    scheduleMicrotask(() async {
+      print('============= ws://100.100.0.1:8888/websocket');
+      _webSocket = await WebSocket.connect("ws://100.100.0.1:8888/websocket");
+      _webSocket.listen(_onReceive, onDone: () {
+        print('连接关闭时响应');
+      }, onError: (error) {
+        print('发生错误');
+      }, cancelOnError: true);
+
+      WriteBuffer buffer = WriteBuffer()
+            ..putUint16(0x8888)
+            // ..putUint8(0x40);
+            ..putUint8(0x0A)
+          // ..putUint8List(Uint8List.fromList(utf8.encode('192.168.5.161 8888')))
+          // ..putUint8List(Uint8List.fromList(utf8.encode('100.100.0.179 7777')))
+          // ..putUint8(0x0B)
+          // ..putUint8List(Uint8List.fromList(utf8.encode('192.168.5.161 18888')))
+          // ..putUint8(0)
+          //
+          ;
+      List<int> bytes = buffer.done().buffer.asUint8List();
+      _webSocket?.add(bytes);
+
+      var receiver = await UDP.bind(Endpoint.unicast(InternetAddress.tryParse("172.27.35.14"), port: Port(7777)));
+      receiver.send(bytes, Endpoint.unicast(InternetAddress.tryParse("100.100.0.1"), port: Port(36081)));
+      receiver.listen((datagram) {
+        mjpeg?.feedData(datagram.data);
+      });
+    });
+  }
+
+  void _onReceive(data) {
+    print("收到服务器数据:" + data.toString());
+    // mjpeg?.feedData(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     final isRunning = useState(true);
+    mjpeg = Mjpeg(
+      isLive: isRunning.value,
+      stream: '',
+      // stream: 'http://192.168.0.86:8080/?action=stream',
+      // stream: 'http://91.133.85.170:8090/cgi-bin/faststream.jpg?stream=half&fps=15&rand=COUNTER', //'http://192.168.1.37:8081',
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Demo Home Page'),
@@ -29,11 +87,7 @@ class MyHomePage extends HookWidget {
         children: <Widget>[
           Expanded(
             child: Center(
-              child: Mjpeg(
-                isLive: isRunning.value,
-                stream:
-                'http://91.133.85.170:8090/cgi-bin/faststream.jpg?stream=half&fps=15&rand=COUNTER', //'http://192.168.1.37:8081',
-              ),
+              child: mjpeg,
             ),
           ),
           Row(
@@ -46,10 +100,7 @@ class MyHomePage extends HookWidget {
               ),
               RaisedButton(
                 onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => Scaffold(
-                            appBar: AppBar(),
-                          )));
+                  test();
                 },
                 child: Text('Push new route'),
               ),
@@ -58,5 +109,66 @@ class MyHomePage extends HookWidget {
         ],
       ),
     );
+  }
+
+  Future<void> test() async {
+    print('test1');
+
+    // for (int i = 0; i < 10; i++) {
+    //   await mouse(1, 10, 10);
+    //   await mouse(0, 10, 10);
+    // }
+
+    await keyboard(34);
+  }
+
+  Future<void> keyboard(int k1, [int k2 = 0, int k3 = 0, int k4 = 0, int k5 = 0, int k6 = 0]) async {
+    // aaaa 0800 0000 0009 0000 0000
+    await Future.delayed(
+        Duration(milliseconds: 10),
+        () => send(WriteBuffer()
+          ..putUint16(0xaaaa)
+          ..putUint8(0x08)
+          ..putUint8(0x0)
+          ..putUint8(k1)
+          ..putUint8(k2)
+          ..putUint8(k3)
+          ..putUint8(k4)
+          ..putUint8(k5)
+          ..putUint8(k6)));
+  }
+
+  Future<void> posMouse(int x, int y) async {
+    // 精准鼠标 aaaa 0551 00ec 56eb 00
+    await Future.delayed(
+        Duration(milliseconds: 10),
+        () => send(WriteBuffer()
+          ..putUint16(0xaaaa)
+          ..putUint8(0x05)
+          ..putUint8(0x51)
+          ..putUint16(0)
+          ..putUint8(x)
+          ..putUint8(y)));
+  }
+
+  Future<void> mouse(int key, int x, int y) async {
+    await Future.delayed(
+        Duration(milliseconds: 10),
+        () => send(WriteBuffer()
+          ..putUint16(0xaaaa)
+          ..putUint8(0x04)
+          ..putUint8(0x20)
+          ..putUint8(key)
+          ..putUint8(x)
+          ..putUint8(y)
+          ..putUint8(0x0)));
+  }
+
+  void send(WriteBuffer buffer) {
+    ByteData data = buffer.done();
+    ByteBuffer dataBuffer = data.buffer;
+    List<int> bytes = dataBuffer.asUint8List().sublist(0, data.lengthInBytes);
+    print('sending $bytes');
+    _webSocket?.add(bytes);
   }
 }
--- lib/src/mjpeg.dart
+++ lib/src/mjpeg.dart
@@ -32,6 +32,8 @@ class _MjpegStateNotifier extends ChangeNotifier {
 
 /// A Mjpeg.
 class Mjpeg extends HookWidget {
+  late final _StreamManager manager;
+
   final String stream;
   final BoxFit? fit;
   final double? width;
@@ -42,7 +44,12 @@ class Mjpeg extends HookWidget {
   final Widget Function(BuildContext contet, dynamic error)? error;
   final Map<String, String> headers;
 
-  const Mjpeg({
+  late final ValueNotifier<MemoryImage?> image;
+  late final _MjpegStateNotifier state;
+  late final _MjpegStateNotifier visible;
+  late final ValueNotifier<dynamic> errorState;
+
+  Mjpeg({
     this.isLive = false,
     this.width,
     this.timeout = const Duration(seconds: 5),
@@ -53,20 +60,23 @@ class Mjpeg extends HookWidget {
     this.loading,
     this.headers = const {},
     Key? key,
-  }) : super(key: key);
+  }) : super(key: key) {
+    image = useState<MemoryImage?>(null);
+    state = useMemoized(() => _MjpegStateNotifier());
+    visible = useListenable(state);
+    errorState = useState<dynamic>(null);
+
+    manager = useMemoized(() => _StreamManager(stream, image, errorState, isLive && visible.visible, headers, timeout),
+        [stream, isLive, visible.visible, timeout]);
+  }
 
   @override
   Widget build(BuildContext context) {
-    final image = useState<MemoryImage?>(null);
-    final state = useMemoized(() => _MjpegStateNotifier());
-    final visible = useListenable(state);
-    final errorState = useState<dynamic>(null);
-    final manager = useMemoized(() => _StreamManager(stream, isLive && visible.visible, headers, timeout), [stream, isLive, visible.visible, timeout]);
     final key = useMemoized(() => UniqueKey(), [manager]);
 
     useEffect(() {
       errorState.value = null;
-      manager.updateStream(context, image, errorState);
+      manager.updateStream(context);
       return manager.dispose;
     }, [manager]);
 
@@ -90,7 +100,10 @@ class Mjpeg extends HookWidget {
     }
 
     if (image.value == null) {
-      return SizedBox(width: width, height: height, child: loading == null ? Center(child: CircularProgressIndicator()) : loading!(context));
+      return SizedBox(
+          width: width,
+          height: height,
+          child: loading == null ? Center(child: CircularProgressIndicator()) : loading!(context));
     }
 
     return VisibilityDetector(
@@ -108,6 +121,12 @@ class Mjpeg extends HookWidget {
       },
     );
   }
+
+  void feedData(List<int> chunk) {
+    DateTime time = DateTime.now();
+    manager._feedData(chunk.sublist(8));
+    print(DateTime.now().difference(time).inMilliseconds);
+  }
 }
 
 class _StreamManager {
@@ -115,6 +134,9 @@ class _StreamManager {
   static const _soi = 0xD8;
   static const _eoi = 0xD9;
 
+  BuildContext? context;
+  final ValueNotifier<MemoryImage?> image;
+  final ValueNotifier<dynamic> errorState;
   final String stream;
   final bool isLive;
   final Duration _timeout;
@@ -122,7 +144,7 @@ class _StreamManager {
   final Client _httpClient = Client();
   StreamSubscription? _subscription;
 
-  _StreamManager(this.stream, this.isLive, this.headers, this._timeout);
+  _StreamManager(this.stream, this.image, this.errorState, this.isLive, this.headers, this._timeout);
 
   Future<void> dispose() async {
     if (_subscription != null) {
@@ -132,10 +154,14 @@ class _StreamManager {
     _httpClient.close();
   }
 
-  Future<void> _sendImage(BuildContext context, ValueNotifier<MemoryImage?> image, ValueNotifier<dynamic> errorState, List<int> chunks) async {
+  Future<void> _sendImage(ValueNotifier<MemoryImage?> image, ValueNotifier<dynamic> errorState, List<int> chunks) async {
+    if (context == null) {
+      return;
+    }
+
     final imageMemory = MemoryImage(Uint8List.fromList(chunks));
     try {
-      await precacheImage(imageMemory, context, onError: (err, trace) {
+      await precacheImage(imageMemory, context!, onError: (err, trace) {
         print(err);
       });
       errorState.value = null;
@@ -143,48 +169,22 @@ class _StreamManager {
     } catch (ex) {}
   }
 
-  void updateStream(BuildContext context, ValueNotifier<MemoryImage?> image, ValueNotifier<dynamic> errorState) async {
+  void updateStream(BuildContext context) async {
+    this.context = context;
+
+    if (stream == '') {
+      return;
+    }
+
     try {
       final request = Request("GET", Uri.parse(stream));
       request.headers.addAll(headers);
-      final response = await _httpClient.send(request).timeout(_timeout); //timeout is to prevent process to hang forever in some case
+      final response =
+          await _httpClient.send(request).timeout(_timeout); //timeout is to prevent process to hang forever in some case
 
       if (response.statusCode >= 200 && response.statusCode < 300) {
-        var _carry = <int>[];
         _subscription = response.stream.listen((chunk) async {
-          if (_carry.isNotEmpty && _carry.last == _trigger) {
-            if (chunk.first == _eoi) {
-              _carry.add(chunk.first);
-              await _sendImage(context, image, errorState, _carry);
-              _carry = [];
-              if (!isLive) {
-                dispose();
-              }
-            }
-          }
-
-          for (var i = 0; i < chunk.length - 1; i++) {
-            final d = chunk[i];
-            final d1 = chunk[i + 1];
-
-            if (d == _trigger && d1 == _soi) {
-              _carry.add(d);
-            } else if (d == _trigger && d1 == _eoi && _carry.isNotEmpty) {
-              _carry.add(d);
-              _carry.add(d1);
-
-              await _sendImage(context, image, errorState, _carry);
-              _carry = [];
-              if (!isLive) {
-                dispose();
-              }
-            } else if (_carry.isNotEmpty) {
-              _carry.add(d);
-              if (i == chunk.length - 2) {
-                _carry.add(d1);
-              }
-            }
-          }
+          _feedData(chunk);
         }, onError: (err) {
           try {
             errorState.value = err;
@@ -200,6 +200,44 @@ class _StreamManager {
     } catch (error) {
       errorState.value = error;
       image.value = null;
+    }
+  }
+
+  var _carry = <int>[];
+
+  Future<void> _feedData(List<int> chunk) async {
+    if (_carry.isNotEmpty && _carry.last == _trigger) {
+      if (chunk.first == _eoi) {
+        _carry.add(chunk.first);
+        await _sendImage(image, errorState, _carry);
+        _carry = [];
+        if (!isLive) {
+          dispose();
+        }
+      }
+    }
+
+    for (var i = 0; i < chunk.length - 1; i++) {
+      final d = chunk[i];
+      final d1 = chunk[i + 1];
+
+      if (d == _trigger && d1 == _soi) {
+        _carry.add(d);
+      } else if (d == _trigger && d1 == _eoi && _carry.isNotEmpty) {
+        _carry.add(d);
+        _carry.add(d1);
+
+        await _sendImage(image, errorState, _carry);
+        _carry = [];
+        if (!isLive) {
+          dispose();
+        }
+      } else if (_carry.isNotEmpty) {
+        _carry.add(d);
+        if (i == chunk.length - 2) {
+          _carry.add(d1);
+        }
+      }
     }
   }
 }
